969f6e144eddb824f4ba12c95f2d6c01b6025d1f
Jana Moudra <jana.moudra@gmail.com>
2012-09-24 03:50:35 +0200

    Removed unneeded files and code

--- src/site/js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var addPermalink = function() {
-  	$(this).addClass('has-permalink');
-  	$(this).append($('<a class="permalink" title="Permalink" href="#' + $(this).attr('id') + '">#</a>'));
-  };
-
-  $.each(['h2','h3','h4'], function(n, h) { $('.has-permalinks ' + h).each(addPermalink); });
-});
