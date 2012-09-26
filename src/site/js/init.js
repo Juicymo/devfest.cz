984a296b24624eb846aeee9e2a94f2c365d1708b
Jana Moudra <jana.moudra@gmail.com>
2012-09-26 02:12:31 +0200

    Added class to register form input. Placeholders for inputs in IE

--- src/site/js/init.js
+++ src/site/js/init.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-	setInterval(function() {
+
+	if ($.browser.msie && $.browser.version <= 9) {
+      var formText = 'Váš e-mail';
+
+      jQuery('.register form .text').val(formText);
+  
+      jQuery('.register form .text').click(function(){
+          if(jQuery(this).val() == formText) {
+              jQuery(this).val('');
+          }
+      });
+  
+      jQuery('.register form .text').blur(function(){
+          if(jQuery(this).val() == '') {
+              jQuery('.register form .text').val(formText);
+          }
+      });
+    }
+
+    setInterval(function() {
 	    var tabs = $('#nav-tabs .nav-tabs > li'),
 	        active = tabs.filter('.active'),
 	        next = active.next('li'),
