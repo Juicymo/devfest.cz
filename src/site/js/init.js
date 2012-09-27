54458f7ffc206c356e47b7fc2bcb3fdc509bce66
Jana Moudra <jana.moudra@gmail.com>
2012-09-27 18:02:37 +0200

    Spinner shown when loading contacting MailChimp

--- src/site/js/init.js
+++ src/site/js/init.js
@@ -1,29 +1,54 @@
 $(document).ready(function(){
 
-	if ($.browser.msie && $.browser.version <= 9) {
+	setInterval(function() {
+    var tabs = $('#nav-tabs .nav-tabs > li'),
+        active = tabs.filter('.active'),
+        next = active.next('li'),
+        toClick = next.length ? next.find('a') : tabs.eq(0).find('a');
+
+    toClick.trigger('click');
+  }, 5000);
+
+  if ($.browser.msie && $.browser.version <= 9) {
       var formText = 'Váš e-mail';
 
-      jQuery('.register form .text').val(formText);
+      $('.register form .text').val(formText);
   
-      jQuery('.register form .text').click(function(){
-          if(jQuery(this).val() == formText) {
-              jQuery(this).val('');
+      $('.register form .text').click(function(){
+          if($(this).val() == formText) {
+              $(this).val('');
           }
       });
   
-      jQuery('.register form .text').blur(function(){
-          if(jQuery(this).val() == '') {
-              jQuery('.register form .text').val(formText);
+      $('.register form .text').blur(function(){
+          if($(this).val() == '') {
+              $('.register form .text').val(formText);
           }
       });
     }
 
-    setInterval(function() {
-	    var tabs = $('#nav-tabs .nav-tabs > li'),
-	        active = tabs.filter('.active'),
-	        next = active.next('li'),
-	        toClick = next.length ? next.find('a') : tabs.eq(0).find('a');
+  if ($('.homepage').size()) {
+    var spinnerDiv, spinnerInterval;   
+    var spinnerVisible = false;
 
-	    toClick.trigger('click');
-	}, 5000);
+    spinnerDiv = document.createElement('div');
+    $(spinnerDiv).attr('class', 'spinner');
+    $('.register form').append(spinnerDiv);
+
+    $('.register form').submit(function(){
+      if (!spinnerVisible) {
+        $(spinnerDiv).show();        
+        spinnerVisible = true;
+        spinnerInterval = setInterval(function() {
+          if (($('.register form .mc-field-group .mce_inline_error').size() > 0)
+            || !$('.register form #mce-responses #mce-error-response:empty').size()
+            || !$('.register form #mce-responses #mce-success-response:empty').size()) {
+            $(spinnerDiv).hide();
+            spinnerVisible = false;
+            clearInterval(spinnerInterval);
+          }
+        }, 500);
+      }      
+    });   
+  }
 });
